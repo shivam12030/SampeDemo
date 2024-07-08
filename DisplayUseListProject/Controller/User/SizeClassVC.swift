c173b4f160b41fbcfc12d5854ea7f2fe072fef1c
Sanjay <pandey.shivam@neosoftmail.com>
2024-07-08 19:02:22 +0530

    add get user profile method

--- DisplayUseListProject/Controller/User/SizeClassVC.swift
+++ DisplayUseListProject/Controller/User/SizeClassVC.swift
@@ -14,9 +14,14 @@ class SizeClassVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        self.getUserProfileData(name: "Shivam")
         buttonCenterConstarint.isActive = true
         buttonBottomConstraint.isActive = false
         // Do any additional setup after loading the view.
+    }
+    
+      // MARK: - User profile
+    func getUserProfileData(name: String) {
+        print("The user name is: \(name)")
     }
 }
